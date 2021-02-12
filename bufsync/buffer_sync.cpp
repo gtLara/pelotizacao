0be56a20638b2bf6504dac7457caca9c7837aaa0
Gabriel Lara <gtlchaves@gmail.com>
2021-02-12 10:48:13 -0300

    Adiciona toggle para threads parte 1

---
+++ bufsync/buffer_sync.cpp
@@ -0,0 +1,278 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <process.h>	// _beginthreadex() e _endthreadex() 
+#include <conio.h>		// _getch
+
+#define _CHECKERROR	1		
+#include "CheckForError.h"
+
+// casting para parâmetros de _beginthreadx
+// TODO: inicializar threads de forma mais "moderna"
+
+typedef unsigned (WINAPI *CAST_FUNCTION)(LPVOID);
+typedef unsigned *CAST_LPDWORD;
+
+/* define teclas para controle de bloqueio de threads */
+
+#define	ESC			0x1B
+#define	tecla_g			0x67
+#define	tecla_c			0x63
+#define	tecla_r			0x72
+#define	tecla_p			0x70
+#define	tecla_a			0x61
+#define	tecla_l			0x6C
+
+/* cria handles para eventos de toggle da parte 1 */ 
+
+HANDLE leitura_medicao_toggle_event;
+HANDLE leitura_dados_toggle_event;
+HANDLE captura_mensagens_toggle_event;
+
+/* cria handles para semaforos necessarios */
+
+HANDLE sem_livre;
+HANDLE sem_ocupado;
+HANDLE sem_rw;
+
+/* cria evento de termino de threads */
+
+HANDLE end_event;
+
+/* declara funcoes para cada thread */
+
+DWORD WINAPI leitura_medicao(LPVOID);
+DWORD WINAPI leitura_dados(LPVOID);
+DWORD WINAPI captura_mensagens(LPVOID);
+
+void show_time();
+
+SYSTEMTIME tempo;
+
+int main()
+{
+	HANDLE thread_leitura_medicao;
+	HANDLE thread_leitura_dados;
+	HANDLE thread_captura_mensagens;
+
+    HANDLE threads[3] = {thread_leitura_medicao, thread_leitura_dados, thread_captura_mensagens};
+	
+	DWORD thread_id;
+	DWORD thread_exit_code = 0;
+
+    /* eventos com reset automatico */
+
+    leitura_dados_toggle_event = CreateEvent(NULL, FALSE, FALSE, NULL);  
+	CheckForError(leitura_dados_toggle_event);
+
+    leitura_medicao_toggle_event = CreateEvent(NULL, FALSE, FALSE, NULL);  
+	CheckForError(leitura_medicao_toggle_event);
+
+    captura_mensagens_toggle_event = CreateEvent(NULL, FALSE, FALSE, NULL);  
+	CheckForError(captura_mensagens_toggle_event);
+
+    /* evento com reset manual */
+
+	end_event = CreateEvent(NULL, TRUE, FALSE, NULL);
+	CheckForError(end_event);
+
+    /* cria threads */
+
+	thread_leitura_medicao= (HANDLE) _beginthreadex(NULL, 0, (CAST_FUNCTION)leitura_medicao, 
+                                            (LPVOID)0,
+                                                    0,
+                                            (CAST_LPDWORD)&thread_id	
+                                            );
+
+    CheckForError(thread_leitura_medicao);
+
+	thread_leitura_dados= (HANDLE) _beginthreadex(NULL, 0, (CAST_FUNCTION)leitura_dados, 
+                                            (LPVOID)0,
+                                                    0,
+                                            (CAST_LPDWORD)&thread_id	
+                                            );
+
+    CheckForError(thread_leitura_dados);
+
+	thread_captura_mensagens = (HANDLE) _beginthreadex(NULL, 0, (CAST_FUNCTION)captura_mensagens, 
+                                            (LPVOID)0,
+                                                    0,
+                                            (CAST_LPDWORD)&thread_id	
+                                            );
+
+    CheckForError(thread_captura_mensagens);
+    
+	if (thread_leitura_medicao) printf("thread leitura de medicao criada com id = %0x \n", thread_id);
+	if (thread_leitura_dados) printf("thread leitura dados criada com id = %0x \n", thread_id);
+	if (thread_captura_mensagens) printf("thread captura mensagens criada com id = %0x \n", thread_id);
+
+    char key;
+
+	printf("\nqualquer tecla para fazer o toggle da leitura de medicao, esc para encerrar:\n");
+	do {
+
+        key = _getch();
+
+        switch(key){
+            case tecla_a:
+                SetEvent(leitura_medicao_toggle_event);
+                break;
+            case tecla_c:
+                SetEvent(leitura_dados_toggle_event);
+                break;
+            case tecla_r:
+                SetEvent(captura_mensagens_toggle_event);
+                break;
+        }
+	} while (key != ESC);
+    
+    /* como ESC foi inserido, sinaliza para finalizar threads */
+
+    SetEvent(end_event);
+
+    /* aguarda termino de threads */ 
+
+    WaitForMultipleObjects(3, threads, FALSE, INFINITE);
+
+	CloseHandle(thread_leitura_medicao);
+	CloseHandle(thread_leitura_dados);
+	CloseHandle(thread_captura_mensagens);
+	CloseHandle(leitura_medicao_toggle_event);
+	CloseHandle(leitura_dados_toggle_event);
+	CloseHandle(captura_mensagens_toggle_event);
+	CloseHandle(end_event);
+
+	printf("\nprocesso encerrando\n");	
+
+    /* TODO: ver como fazer main esperar por termino de thread. aqui isso nao parece ter acontecido. */
+
+	return EXIT_SUCCESS;
+}  // main
+
+
+
+DWORD WINAPI leitura_medicao(LPVOID id)
+{	
+	HANDLE Events[2]= {leitura_medicao_toggle_event, end_event};
+	DWORD ret;
+	int event_id= 0;
+
+	do {
+
+        show_time();
+
+        /* espera por 1 s por objeto de toggle ou finalizador */
+
+		ret = WaitForMultipleObjects(2, Events, FALSE, 1000);
+
+        /* se tiver esperado o tempo limite, prosseguir com logica */
+
+        if(ret == WAIT_TIMEOUT){ 
+            continue;
+        }
+
+        /* se nao, executar comportamento demandado */
+
+		event_id = ret - WAIT_OBJECT_0;
+		if (event_id == 0){
+
+			printf("\n thread leitura granulometria bloqueada. para desbloquear, aperte uma outra tecla qualquer \n");
+            WaitForSingleObject(leitura_medicao_toggle_event, INFINITE);
+			printf("\n thread leitura granulometria desbloqueada.\n");
+
+		}
+
+	} while (event_id != 1);
+        
+	_endthreadex((DWORD) 0);
+
+	return(0);
+} 
+
+DWORD WINAPI leitura_dados(LPVOID id)
+{	
+    HANDLE Events[2]= {leitura_dados_toggle_event, end_event};
+	DWORD ret;
+	int event_id= 0;
+
+
+	do {
+
+        show_time();
+
+        /* espera por 1 s por objeto de toggle ou finalizador */
+
+		ret=WaitForMultipleObjects(2, Events, FALSE, 1000);
+
+        /* se tiver esperado o tempo limite, prosseguir com logica */
+
+        if(ret == WAIT_TIMEOUT){ 
+            continue;
+        }
+
+        /* se nao, executar comportamento demandado */
+
+		event_id = ret - WAIT_OBJECT_0;
+		if (event_id == 0){
+
+			printf("\n thread leitura dados bloqueada. para desbloquear, aperte uma outra tecla qualquer \n");
+            WaitForSingleObject(leitura_dados_toggle_event, INFINITE);
+			printf("\n thread leitura dados desbloqueada.\n");
+
+		}
+
+	} while (event_id != 1);
+        
+	_endthreadex((DWORD) 0);
+
+	return(0);
+} 
+
+DWORD WINAPI captura_mensagens(LPVOID id)
+{	
+	HANDLE Events[2]= {captura_mensagens_toggle_event, end_event};
+	DWORD ret;
+	int event_id= 0;
+
+
+	do {
+
+        show_time();
+
+        /* espera por 1 s por objeto de toggle ou finalizador */
+
+		ret=WaitForMultipleObjects(2, Events, FALSE, 1000);
+
+        /* se tiver esperado o tempo limite, prosseguir com logica */
+
+        if(ret == WAIT_TIMEOUT){ 
+            continue;
+        }
+
+        /* se nao, bloquear */
+
+		event_id = ret - WAIT_OBJECT_0;
+		if (event_id == 0){
+
+			printf("\n thread captura mensagens bloqueada. para desbloquear, aperte uma outra tecla qualquer \n");
+            WaitForSingleObject(captura_mensagens_toggle_event, INFINITE);
+			printf("\n thread captura mensagens desbloqueada.\n");
+
+		}
+
+    /* enquanto Esc nao for pressionado */
+
+	} while (event_id != 1); 
+        
+	_endthreadex((DWORD) 0);
+
+	return(0);
+} 
+
+void show_time()
+{	
+
+	GetLocalTime(&tempo);
+	printf("\n %02d:%02d:%02d \n ", tempo.wHour, tempo.wMinute, tempo.wSecond);
+
+};
